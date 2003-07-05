7e9e6835bb4aa741b1116cac9d47ce4091d5ec77
lnu <lnu>
2003-07-05 20:01:18 +0000

    logfilter and testbed moved too tools/

--- contrib/logfilter/logfilter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "generating $1.out.gz ... this can take some time !"
-grep "^\[.*\] <IRC : " $1 | sed -e 's/\[.*\] <IRC : \(.*\)/\1/' | gzip > $1.out.gz
-echo "$1.out.gz generated. respective sizes :"
-ls -lh $1 $1.out.gz
-echo "now please send $1.out.gz to lucas@lucas-nussbaum.net,"
-echo "explaining how the crash occured, if possible"
-echo "thanks for your help improving thales !"
