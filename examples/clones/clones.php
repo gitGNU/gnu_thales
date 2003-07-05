860b0431b2c5f2acb19ec77072262a19b0af530e
lnu <lnu>
2003-07-05 20:13:55 +0000

    clones moved to examples/

--- examples/clones/clones.php
+++ examples/clones/clones.php
@@ -1,7 +1,24 @@
 <?php
+/*  Clones detection script
+ *  Copyright (C) 2002 Jens 'DukePyrolator' Voss <DukePyrolator@wiredirc.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
 /* ============================================================================
  * clone-detecting-script www (powered by Thales)
- * (note: this is just an example script, add/modify as you wish)
  *
  *    File:      clones.php
  *    Author:    Jens 'DukePyrolator' Voss | DukePyrolator@wiredirc.de
@@ -9,9 +26,6 @@
  *    Input:     mySQL data from Thales tables
  *    Output:    all clones on chan (a la mIRC)
  *    Usage:     htp://.../clones.php?chan=mychan (without the leading '#')
- *
- *
- *
  *
  * ===========================================================================
  */
