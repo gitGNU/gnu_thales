d1a7077965389b9af3bb8f2a1870e34958dd2b29
lnu <lnu>
2004-01-18 15:03:50 +0000

    swhois now work.

--- src/messages.c
+++ src/messages.c
@@ -373,7 +373,7 @@ static void m_sdesc(char *source, int ac, char **av)
 /* SWHOIS - forwarded */
 static void m_swhois(char *source, int ac, char **av)
 {
-	do_swhois(source, av[0]);
+	do_swhois(av[0], av[1]);
 }
 
 
