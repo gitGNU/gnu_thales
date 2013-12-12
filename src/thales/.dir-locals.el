d1e34dd9e087930ea75e9a980c4b0ca57f9281d5
Dmitry Bogatov <KAction@gnu.org>
2013-12-12 14:23:25 -0600

    Add emacs-specific highlighting for macroses

---
+++ src/thales/.dir-locals.el
@@ -0,0 +1,15 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((scheme-mode
+     (eval font-lock-add-keywords nil
+	 `(("(\\(sealed\\)\s\\(\\w+\\)"
+	       (1 font-lock-keyword-face)
+	       (2 font-lock-constant-face))
+	   ("\\<lambda-match\\>" . font-lock-keyword-face)
+           ("(\\(define-match\\)\s+(\\(\\w+\\)"
+	       (1 font-lock-keyword-face)
+	       (2 font-lock-function-name-face))
+           ("\\<for\\*?\\>" . font-lock-keyword-face)
+)
+	 :lowest-priority)))
