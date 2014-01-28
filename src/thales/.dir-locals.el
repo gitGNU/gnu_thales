74d2dfeaf65fd49d007607b43e4f09930ef515ec
Dmitry Bogatov <KAction@gnu.org>
2014-01-28 11:45:40 +0400

    Add emacs highlighing for macroses

--- src/thales/.dir-locals.el
+++ src/thales/.dir-locals.el
@@ -3,13 +3,15 @@
 
 ((scheme-mode
      (eval font-lock-add-keywords nil
-	 `(("(\\(sealed\\)\s\\(\\w+\\)"
+	 `(("(\\(sealed\\|throw\\|catch\\)\s'?\\(\\w+\\)"
 	       (1 font-lock-keyword-face)
 	       (2 font-lock-constant-face))
 	   ("\\<lambda-match\\>" . font-lock-keyword-face)
            ("(\\(define-match\\)\s+(\\(\\w+\\)"
 	       (1 font-lock-keyword-face)
 	       (2 font-lock-function-name-face))
-           ("\\<for\\*?\\>" . font-lock-keyword-face)
+           ("\\<for\\*?\\>"   . font-lock-keyword-face)
+	   ("\\<lambda\\*\\>" . font-lock-keyword-face)
+	   ("error:.*?\\_>"   . 'bold)
 )
 	 :lowest-priority)))
