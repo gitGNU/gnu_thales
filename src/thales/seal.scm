22072b901376cf11cd5bf32c28785395de39fa64
Dmitry Bogatov <KAction@gnu.org>
2014-01-28 16:12:30 +0400

    Make (thales seal) self-contained and minimize dependencies.

--- src/thales/seal.scm
+++ src/thales/seal.scm
@@ -1,24 +1,42 @@
 (define-module (thales seal)
     #:export (sealed))
-(use-modules (thales syntax))
 (use-modules (ice-9 match))
 (use-modules (ice-9 pretty-print))
 (use-modules (srfi srfi-1))
 (use-modules (srfi srfi-26))
+
+(define-syntax push
+    (syntax-rules ()
+        ((_ val list)
+         (set! list (cons val list)))))
 
 (define (current-module-name)
     (module-name (current-module)))
 (define (current-module-load-filename)
     (string-join (map symbol->string (current-module-name)) "/"))
 
+(define (seal:pretty-print obj)
+    "Output object on stderr if env THALES_SEAL is set.
+
+This function is used to output valid Guile program to check seals."
+    (when (getenv "THALES_SEAL")
+	(format (current-error-port) "~a"
+		(with-output-to-string [cute pretty-print obj]))))
+
+(define (in-module?)
+    (module-filename (current-module)))
+
 (define load-self-once
     (let ((already-loaded '()))
-        (lambda ()
-            "Load current module, unless it was not already
+	(lambda ()
+	    "Load current module, unless it was not already
 loaded with this function."
-            (unless (member (current-module) already-loaded)
-                (primitive-load-path (current-module-load-filename))
-                (push (current-module) already-loaded)))))
+	    (unless (member (current-module) already-loaded)
+		(seal:pretty-print `(use-modules ,(current-module-name)))
+		(seal:pretty-print `(use-modules (thales seal)))
+		(when (in-module?)
+		    (primitive-load-path (current-module-load-filename)))
+		(push (current-module) already-loaded)))))
 
 (define (error:broken-seal form expect result)
     (format #t "\nCompilation aborted: seal broken when evaluating\n")
@@ -33,7 +51,7 @@ loaded with this function."
     (syntax-rules ()
         ((_ <form> <on-values> <on-throw>)
          (let ((throw-handler-called #f))
-             (call-with-values #[catch #t
+             (call-with-values [cute catch #t
                                        (lambda () <form>)
                                        (lambda args
                                            (apply <on-throw> args)
@@ -46,7 +64,7 @@ loaded with this function."
     (syntax-rules ()
 	([_ <form> (<val> ...)]
 	 (let* ((expected (list 'values <val> ...))
-		(error:handle #[error:broken-seal '<form> expected]))
+		(error:handle [cute error:broken-seal '<form> expected]))
 	     (call-and-catch <form>
 	         (lambda args
 		     (unless (equal? args (cdr expected))
@@ -58,7 +76,7 @@ loaded with this function."
     (syntax-rules ()
 	([_ <form> (<val> ...)]
 	 (let* ((expected (list 'throw <val> ...))
-		(error:handle #[error:broken-seal '<form>
+		(error:handle [cute error:broken-seal '<form>
                                     (append expected '(....))]))
 	     (call-and-catch <form>
                  (lambda args
@@ -90,6 +108,10 @@ loaded with this function."
              (with-syntax ((& (datum->syntax env '&)))
                  #'(eval-when (compile)
 		       (load-self-once)
+		       (when (module-variable (module-public-interface
+					       (current-module))
+					      'f)
+			   (seal:pretty-print `(sealed ,'f (obj ...) ...)))
 		       (let ((& f))
 			   (format #t "Checking seals with & = ~a... " 'f)
 			   (seal-clause f obj ...) ...
