d744676b2b107ee11cfc629c4464c51fe153425e
Dmitry Bogatov <KAction@gnu.org>
2014-02-15 22:09:50 +0400

    Fix overflows in documentation

--- doc/examples/overview-fixed.scm
+++ doc/examples/overview-fixed.scm
@@ -6,9 +6,13 @@
 (use-modules (thales seal))
 
 (sealed mystic
-	(1 2 *** (- 4 1))	;; Short-cut for most common case. Expected form is evaluated
+	(1 2 *** (- 4 1))	;; Short-cut for most common case.
+				;; Expected form is evaluated
+
 	(1 2 *+* 3)		;; Same. Expected form is not evaluated.
-	((& 5 6) => 11)         ;; Full form. & is bound to function beeing testing.
+	((& 5 6) => 11)		;; Full form.
+				;; & is bound to function beeing testing.
+
 	((mystic 3 4) => 7)	;; Or it can be written in full
 	(1 2 *** 3))		;; This will no more fail
 
@@ -23,4 +27,3 @@
 (define (not-exported x y)
     "Some not-exported function"
     (/ y x))
-
--- doc/examples/overview.scm
+++ doc/examples/overview.scm
@@ -5,9 +5,13 @@
 (use-modules (thales seal))
 
 (sealed mystic
-	(1 2 *** (- 4 1))	;; Short-cut for most common case. Expected form is evaluated
+	(1 2 *** (- 4 1))	;; Short-cut for most common case.
+				;; Expected form is evaluated
+
 	(1 2 *+* 3)		;; Same. Expected form is not evaluated.
-	((& 5 6) => 11)         ;; Full form. & is bound to function beeing testing.
+	((& 5 6) => 11)         ;; Full form.
+				;; & is bound to function beeing testing.
+
 	((mystic 3 4) => 7)	;; Or it can be written in full
 	(1 2 ** 4))		;; This will fail and abort compilation
 
